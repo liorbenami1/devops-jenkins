154b8432fc4095f553605fe480808437b6436262
liorbenami <58229008+liorbenami1@users.noreply.github.com>
2021-02-09 18:05:32 +0200

    Update nexus-pipeline.groovy

--- my-pipelines/nexus-pipeline.groovy
+++ my-pipelines/nexus-pipeline.groovy
@@ -10,7 +10,18 @@ pipeline {
             steps {
                 sh 'mvn clean package'
                 junit '**/target/surefire-reports/TEST-*.xml'
-                archiveArtifacts artifacts: 'target/*.war', fingerprint: true
+                //archiveArtifacts artifacts: 'target/*.war', fingerprint: true
+                nexusPublisher nexusInstanceId: 'my-repo', 
+                    nexusRepositoryId: 'my-maven2', 
+                    packages: [[$class: 'MavenPackage', 
+                                mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'target/']], 
+                                mavenCoordinate: [artifactId: 'simple-maven-project-with-tests', 
+                                                  groupId: 'org.apache.maven', 
+                                                  packaging: 'simple-maven-project-with-tests-1.0-SNAPSHOT', 
+                                                  version: '1.0']
+                               ]
+                              ], 
+                    tagName: 'DEV'
             }
         }
     }
